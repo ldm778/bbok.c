2c7af77d103cd00ec7488ef9968e1e07219e5d94
复合肥 <614945754@qq.com>
2021-12-14 00:47:52 +0800

    测试

---
+++ sfd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int day=0;
+scanf("%d",&day);
+switch(day)
+{
+case 1:
+	printf("星期1\n");
+case 2:
+	printf("星期2\n");
+case 3:
+	printf("星期4\n");
+case 4:
+	printf("星期4\n");
+case 5:
+	printf("星期5\n");
+}
+
+return 0;
+
+}
